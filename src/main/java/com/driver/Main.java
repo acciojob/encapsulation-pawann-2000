6daad2713cc0e421585ff2a470343f500f36f976
pawann-2000 <github.3ljwu@ggmaill.aleeas.com>
2024-02-10 20:51:00 +0530

    Fixed object creation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly a = new RWOnly();
+        a.setName("Hey");
+        System.out.println(a.getName());
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    void setName(String name){
+        this.name = name;
+    }
 }
